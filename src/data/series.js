65e35d2da577e8dc73b21fc73d1875f5b4cd90ef
Silvia Debussi <shivialollol@gmail.com>
2026-05-17 21:27:48 -0300

    imagens corretas

--- src/data/series.js
+++ src/data/series.js
@@ -1,33 +1,34 @@
+import stranger from "../assets/stranger.jpg"
+import dark from "../assets/dark.jpg"
+import breakingbad from "../assets/breakingbad.jpg"
+import tlou from "../assets/tlou.jpg"
+
 export const series = [
   {
     title: "Stranger Things",
     genre: "Sci-Fi",
     rating: "9.0",
-    image:
-      "https://images.unsplash.com/photo-1489599849927-2ee91cede3ba?q=80&w=1200"
+    image: stranger
   },
 
   {
     title: "Dark",
     genre: "Mystery",
     rating: "8.7",
-    image:
-      "https://images.unsplash.com/photo-1517604931442-7e0c8ed2963c?q=80&w=1200"
+    image: dark
   },
 
   {
     title: "Breaking Bad",
     genre: "Drama",
     rating: "9.5",
-    image:
-      "https://images.unsplash.com/photo-1524985069026-dd778a71c7b4?q=80&w=1200"
+    image: breakingbad
   },
 
   {
     title: "The Last of Us",
     genre: "Action",
     rating: "8.9",
-    image:
-      "https://images.unsplash.com/photo-1513106580091-1d82408b8cd6?q=80&w=1200"
+    image: tlou
   }
 ]
